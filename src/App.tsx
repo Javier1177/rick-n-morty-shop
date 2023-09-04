4f2f4b5b5dd3740cb6ec5a96a2b37f49f648bde9
Javi <javierlopezblasco@gmail.com>
2023-09-04 12:32:35 +0200

    Modify styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Home from './pages/Home';
 
 function App() {
 	const fetchProducts = useProductList(state => state.fetchProductList);
-	const productList = useProductList(state => state.productList);
 
 	useEffect(() => {
 		fetchProducts();
--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,11 +1,23 @@
+import { useProductList } from '../../store/productsList/productsList';
 import ProductCard from '../ProductCard';
+import Spinner from '../Spinner';
 
-const baseClass = 'ProductList';
+import './productList.style.scss';
+
+const baseClass = 'productList';
 
 const ProductList = () => {
+	const productList = useProductList(state => state.productList);
+	if (!productList.length) return <Spinner />;
+
 	return (
 		<div className={`${baseClass}-container`}>
-			<ProductCard />
+			<div>asdad</div>
+			<div className={`${baseClass}-cards`}>
+				{productList.map(product => (
+					<ProductCard product={product} key={product.id} />
+				))}
+			</div>
 		</div>
 	);
 };
